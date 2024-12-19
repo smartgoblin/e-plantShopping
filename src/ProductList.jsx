9c7c0c118ae4d070d13b2c5bfd66bec9f126a3b9
charly <lypacsprl@outlook.com>
2024-12-19 09:00:56 +0100

    Display products

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -4,6 +4,7 @@ import CartItem from './CartItem';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [addedToCart, setAddedToCart] = useState({}); // State for the added plants to the cart
 
     const plantsArray = [
         {
@@ -268,7 +269,21 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
-
+        {plantsArray.map((item, index) => (
+            <div key={index}>
+                <h1><div>{item.category}</div></h1>
+                <div className="product-list">
+                {item.plants.map((plant, plantIndex) => (
+                    <div className="product-card" key={plantIndex}>
+                        <img className="product-image" src={plant.image} alt={plant.name} />
+                        <div className="product-title">{plant.name}</div>
+                        {/*Similarly like the above plant.name show other details like description and cost*/}
+                        <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                    </div>
+                ))}
+                </div>
+            </div>
+        ))}
 
         </div>
  ) :  (
