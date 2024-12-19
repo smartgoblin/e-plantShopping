7e7d59e76ff0c30ada186840bce0e87f1898fdd6
charly <lypacsprl@outlook.com>
2024-12-19 14:53:18 +0100

    remove button when platn added to cart

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -265,6 +265,15 @@ const handlePlantsClick = (e) => {
     e.preventDefault();
     setShowCart(false);
   };
+
+
+  const isInCart = (name) => {
+      console.log(name);
+      console.log(cartItems);
+     return cartItems.some(item => item.name === name);
+  };
+
+
     return (
         <div>
              <div className="navbar" style={styleObj}>
@@ -310,8 +319,13 @@ const handlePlantsClick = (e) => {
                     <div className="product-card" key={plantIndex}>
                         <img className="product-image" src={plant.image} alt={plant.name} />
                         <div className="product-title">{plant.name}</div>
-                        {/*Similarly like the above plant.name show other details like description and cost*/}
-                        <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                        <div className="product-description">{plant.description}</div>
+                        <div className="product-cost">${plant.cost}</div>
+                        {isInCart(plant.name)?(
+                            <></>
+                        ):(
+                            <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+                        )}
                     </div>
                 ))}
                 </div>
